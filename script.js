4fcd7adfc8a020e5749f73d3758ee8f7e83f5e5e
Eros Linnyker <54625876+LinnykerEros@users.noreply.github.com>
2022-03-29 15:46:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const linhas = document.querySelectorAll(".linhas");
+let inputCheck = document.getElementById("inputCheck");
 let checkTurno = true;
+let fimDeJogo = false;
 const jogadorX = "X";
 const jogadorO = "O";
 const combinacoes = [
@@ -16,11 +18,45 @@ const combinacoes = [
 document.addEventListener("click", (evento) => {
   //se o clique for em "linhas"
   if (evento.target.matches(".linhas")) {
-    //passo o id da div "linhas" ex: linhas[0], linhas[1] para a função jogar;
-    jogar(evento.target.id);
+    //verifco se quero jogar contra o bot ou não
+    if (inputCheck.checked == false) {
+      //passo o id da div "linhas" ex: linhas[0], linhas[1] para a função jogar;
+      jogar(evento.target.id);
+    } else {
+      jogarBot(evento.target.id, jogadorX);
+      setTimeout(() => {
+        jogadorBot();
+      }, 500);
+    }
   }
 });
 
+function jogadorBot() {
+  let posicoesDisp = [];
+  //percorro as linhas na minha grid
+  for (index in linhas) {
+    //verifico se index é número
+    if (!isNaN(index)) {
+      //se for um número, verifico se minha linha com id tal, já está preenchida com X or O
+      if (
+        !linhas[index].classList.contains(jogadorX) &&
+        !linhas[index].classList.contains(jogadorO)
+      ) {
+        //coloco na minha lista as posições disponiveis
+        posicoesDisp.push(index);
+      }
+    }
+  }
+  //gerando uma posição aleatória que é o ID
+  let posicaoAleatoria = Math.floor(Math.random() * posicoesDisp.length);
+  //estou passando para minha função "jogar" os parametros posicoesDisp[posicaoAleatoria] que é o id da proxima posição onde o jogadorO(que é o bot), vai jogar.
+  //também estou identificando se não for fimdejogo o bot joga novamente, se for, ele não joga.
+  if (!fimDeJogo) {
+    jogarBot(posicoesDisp[posicaoAleatoria], jogadorO);
+  }
+}
+
+//função para jogar contra humano, onde preciso checkar qual o turno
 function jogar(id) {
   //pego o id da minha div "linhas"
   const linha = document.getElementById(id);
@@ -33,6 +69,23 @@ function jogar(id) {
   //faço minha div linha[id] receber o turno do jogador, escrevo na minha div X or O
   linha.innerHTML = turnoDoJogador;
 
+  if (turnoDoJogador == jogadorO) {
+    linha.style.color = "hsl(52, 100%, 62%)";
+  }
+  //adiciono qual jogador jogou na class da minha div linha, ex:linhas O ou linhas X
+  linha.classList.add(turnoDoJogador);
+  //passo para a minha função de checar o vencedor, o turno do jogador.
+  checkVencedor(turnoDoJogador);
+}
+//função para jogar contra o bot, não preciso checkar o turno já que está jogando contra bot.
+function jogarBot(id, turnoDoJogador) {
+  //pego o id da minha div "linhas"
+  console.log(`>>>>> ${turnoDoJogador}`);
+  const linha = document.getElementById(id);
+  //criando uma elevação no meu botão quando ele é clicado.
+  linha.style.transform = "scale(1.1)";
+  //faço minha div linha[id] receber o turno do jogador, escrevo na minha div X or O
+  linha.innerHTML = turnoDoJogador;
   if (turnoDoJogador == jogadorO) {
     linha.style.color = "hsl(52, 100%, 62%)";
   }
@@ -56,7 +109,7 @@ function checkVencedor(turnoDoJogador) {
     encerraJogo(turnoDoJogador);
   } else if (checkEmpate()) {
     encerraJogo();
-  } else {
+  } else if (inputCheck.checked == false) {
     checkTurno = !checkTurno;
   }
 }
@@ -65,6 +118,7 @@ function encerraJogo(vencedor = null) {
   let res = document.getElementById("resultado");
   const h2 = document.createElement("h2");
   const h3 = document.createElement("h3");
+  fimDeJogo = true;
   res.style.display = "block";
   res.appendChild(h2);
   res.appendChild(h3);
@@ -74,6 +128,9 @@ function encerraJogo(vencedor = null) {
 
     button.style.transform = "scale(1.1)";
   } else {
+    let button = document.querySelector(".button");
+
+    button.style.transform = "scale(1.1)";
     h2.innerHTML = `Empatou`;
   }
 }
@@ -98,7 +155,8 @@ function checkEmpate() {
   //verifico se a soma dos elementos é igual a 9 que corresponde ao tamanho da minha grid, se sim, significa empate, então ele retorna true.
   return x + o === 9 ? true : false;
 }
-//resetar o jogo
+
 function resetar() {
   location.reload();
 }
+
